e63cf55e435fa5cea521a4fe5f161b4bc7ea609c
murbanojuno <mariaj@junojourney.com>
2025-01-14 23:50:58 +0100

    Update EditNumberModal.tsx

--- frontend/src/features/numbers/components/EditNumberModal.tsx
+++ frontend/src/features/numbers/components/EditNumberModal.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   Box,
-  Button,
   Dialog,
   Stack,
   TextField,
